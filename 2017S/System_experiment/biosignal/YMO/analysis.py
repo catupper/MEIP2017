8f152dcfbe12d015d21899f3e075cdb1cc897e19
Ken Ogura <nekarugo628@gmail.com>
2017-04-13 15:47:30 +0900

    add expdata

---
+++ 2017S/System_experiment/biosignal/YMO/analysis.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pylab as plt
+
+data = []
+
+heltz = 12500.0
+
+x = []
+y = []
+
+with  open("muishiki1.dat", "r") as f:
+    for line in f:
+        t, d = map(float, line.split(' '))
+        
+        if(4.5 <= t <= 5.6):
+            data.append(d)
+N = len(data)
+
+hammingWindow = np.hamming(N)
+hanningWindow = np.hanning(N)
+bartlettWindow = np.bartlett(N)
+blackmanWindow = np.blackman(N)
+
+tran = np.fft.fft(data*hammingWindow)
+
+
+x = []
+y = []
+tmpx = 0
+cnt = 0
+with open("muishiki1_trans.dat", "w") as f:
+    for d in tran:
+        tmpx = heltz / (len(tran)) * cnt
+        cnt += 1
+        if(tmpx > 200):continue
+
+        x.append(tmpx)
+        y.append(np.abs(d))
+
+print len(x)
+plt.plot(x, y, color = "k")
+
+plt.show()
+
